7c07a10cd8e9ea8415356a995a9be0ebf2ffbf0a
Caio Porto <caioportoramos@gmail.com>
2022-02-03 21:33:08 -0300

    Atividade sem funcionar. Vou estudar mais para entregar certo

--- modulo3/estados-insta4/insta4/src/App.js
+++ modulo3/estados-insta4/insta4/src/App.js
@@ -1,18 +1,65 @@
-import React from 'react'
-import styled from 'styled-components'
-import Post from './components/Post/Post'
+import React from "react";
+import styled from "styled-components";
+import Post from "./components/Post/Post";
 
 const MainContainer = styled.div`
   display: flex;
   justify-content: center;
   flex-direction: column;
   align-items: center;
-`
+`;
 
 class App extends React.Component {
+  state = {
+    users: [
+      {
+        name: "Lalaíne",
+        userPhoto: "https://picsum.photos/51/50",
+        photoPost: "https://picsum.photos/200/150",
+      },
+
+      {
+        name: "CornoValue",
+        userPhoto: "https://picsum.photos/51/50",
+        photoPost: "https://picsum.photos/200/150",
+      },
+
+      {
+        name: "PinkMila",
+        userPhoto: "https://picsum.photos/51/50",
+        photoPost: "https://picsum.photos/200/150",
+      },
+    ]
+  };
+
+transformInVar = () =>{
+
+  const sameUsers = [ ...this.state.users];
+  this.setState({people: users);
+};
+
+
   render() {
+    const usersList = this.state.sameUsers.map((person) => {
+      return (
+        <p>
+          {person.name}</p>
+         <img>{person.userPhoto}</img> 
+         <img >{person.photoPost}</img> 
+        
+      );
+    });
+
     return (
       <MainContainer>
+        <Post>{usersList}</Post>
+      </MainContainer>
+    );
+  }
+}
+
+/* 
+      { <MainContainer>
         <Post
           nomeUsuario={'paulinha'}
           fotoUsuario={'https://picsum.photos/51/50'}
@@ -30,8 +77,8 @@ class App extends React.Component {
           fotoPost={'https://picsum.photos/200/89'}
         />
       </MainContainer>
-    )
+    )  }
   }
-}
+}*/
 
-export default App
+export default App;
