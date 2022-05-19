304dbc5ce2506a00c47be1df2dc44f68a349c7b5
Caio Porto <caioportoramos@gmail.com>
2022-05-19 07:59:18 -0300

    primeiros testes implementados

---
+++ modulo4/intro-testes/testes-unitarios/src/exercicios/ex1.test.js
@@ -0,0 +1,45 @@
+import { checaBissexto } from "./ex1";
+
+describe("Checa bissexto", () => {
+  test("retorna true pra 1600", () => {
+    const resultado = checaBissexto(1600);
+
+    expect(resultado).toEqual(true);
+  });
+
+  test("retorna true pra 2000", () => {
+    const resultado = checaBissexto(2000);
+
+    expect(resultado).toEqual(true);
+  });
+
+  test("retorna true pra 1996", () => {
+    const resultado = checaBissexto(1996);
+
+    expect(resultado).toEqual(true);
+  });
+
+  test("retorna true pra 2008", () => {
+    const resultado = checaBissexto(2008);
+
+    expect(resultado).toEqual(true);
+  });
+
+  test("retorna false pra 2007", () => {
+    const resultado = checaBissexto(2007);
+
+    expect(resultado).toEqual(false);
+  });
+
+  test("retorna false pra 1989", () => {
+    const resultado = checaBissexto(1989);
+
+    expect(resultado).toEqual(false);
+  });
+
+  test("retorna true pra 1990", () => {
+    const resultado = checaBissexto(1990);
+
+    expect(resultado).toEqual(false);
+  });
+});
---
+++ modulo4/intro-testes/testes-unitarios/src/exercicios/ex2.test.js
@@ -0,0 +1,12 @@
+import { checaPalindromo } from "./ex2";
+
+describe("Checa Palíndromo", () => {
+  it("retorna true para 'mirim'", () => {
+    const ehPalindromo = checaPalindromo("mirim");
+    expect(ehPalindromo).toEqual(true);
+  });
+  it("retorna true para 'arara'", () => {
+    const ehPalindromo = checaPalindromo("arara");
+    expect(ehPalindromo).toEqual(true);
+  });
+});
