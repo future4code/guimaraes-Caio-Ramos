dd23d6d13f35aca42370b397566c979489897cf0
Caio Porto <caioportoramos@gmail.com>
2022-05-24 18:07:44 -0300

    exercicio 2 concluido

--- modulo5/intro-typescript/src/ex2.ts
+++ modulo5/intro-typescript/src/ex2.ts
@@ -2,10 +2,8 @@ function imprimeTresCoresFavoritas(
   cor1: string,
   cor2: string,
   cor3: string
-): void {
-  cor1 = prompt("Insira sua primeira cor favorita");
-  cor2 = prompt("Insira sua segunda cor favorita");
-  cor3 = prompt("Insira sua terceira cor favorita");
-  let arrayCor: string[] = [cor1, cor2, cor3];
-  console.log(arrayCor);
+): string[] {
+  let colorArray = [cor1, cor2, cor3];
+  return colorArray;
 }
+console.log(imprimeTresCoresFavoritas("Vermelho", "Verde", "Lilás"));
