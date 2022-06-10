f4a1c6791c88836be7f408c6eb544d06d6f64312
Caio Porto <caioportoramos@gmail.com>
2022-06-10 16:46:56 -0300

    Ex4 OK

--- modulo5/apis-rest/atividade-apis-rest/src/index.ts
+++ modulo5/apis-rest/atividade-apis-rest/src/index.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import { AddressInfo } from "net";
 import { users, Users, UserType } from "./data";
-
+import { v4 as generateId } from "uuid";
 //Middlewares
 const app = express();
 app.use(express.json());
@@ -64,6 +64,36 @@ app.get("/usersbyname", (req: Request, res: Response) => {
       throw new Error("Nome passado errado. Tente novamente.");
     }
     res.status(200).send(userName);
+  } catch (error: any) {
+    res.status(errorCode).send(error.message);
+  }
+});
+//Ex4:
+//A) Utilizando o "put" o usuário é criado, pois ainda não existia
+//B) O método "put" em princípio é usado para atualizar o objeto, porém substitui o post caso não haja um objeto daquele tipo criado
+app.put("/users", (req: Request, res: Response) => {
+  let errorCode = 500;
+  try {
+    const { name, email, type, age } = req.body;
+    if (!name || !email || !type || !age) {
+      errorCode = 422;
+      throw new Error("Algum parâmetro está faltando");
+    }
+    if (type !== UserType.NORMAL && type !== UserType.ADMIN) {
+      errorCode = 422;
+      throw new Error(
+        "Tipo não aceito. Inserir tipo de usuário válido: NORMAL ou ADMIN."
+      );
+    }
+    users.push({
+      id: users.length + 1,
+      name: name,
+      email: email,
+      type: type,
+      age: age,
+    });
+    //Falta inserir o res sua anta
+    res.status(200).send(users);
   } catch (error: any) {
     res.status(errorCode).send(error.message);
   }
