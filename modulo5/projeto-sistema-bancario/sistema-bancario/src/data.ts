6edeaf38e339b08b447a35107115c49ac45efb4a
Caio Porto <caioportoramos@gmail.com>
2022-06-13 16:30:43 -0300

    Ajustes no CPF

--- modulo5/projeto-sistema-bancario/sistema-bancario/src/data.ts
+++ modulo5/projeto-sistema-bancario/sistema-bancario/src/data.ts
@@ -1,21 +1,21 @@
-type Account = {
+import { v4 as generateId } from "uuid";
+
+export type Account = {
   id: string;
   name: string;
-  cpf: number;
+  cpf: number | string;
   dateOfBirth: string;
   balance: number;
   bankStatement: [{ value: number; date: string; description: string }];
 };
 
-/* type BankStatement = {
-    
-  } */
+
 
 export const bankUsers: Account[] = [
   {
     id: "1",
     name: "Caio",
-    cpf: Math.random(),
+    cpf: generateId(),
     dateOfBirth: "19/07/1989",
     balance: 0,
     bankStatement: [{ value: 0, date: "", description: "" }],
@@ -23,7 +23,7 @@ export const bankUsers: Account[] = [
   {
     id: "2",
     name: "Lethícia",
-    cpf: Math.random(),
+    cpf: generateId(),
     dateOfBirth: "09/08/1990",
     balance: 0,
     bankStatement: [{ value: 0, date: "", description: "" }],
@@ -31,7 +31,31 @@ export const bankUsers: Account[] = [
   {
     id: "3",
     name: "Camila",
-    cpf: Math.random(),
+    cpf: generateId(),
+    dateOfBirth: "19/07/1989",
+    balance: 0,
+    bankStatement: [{ value: 0, date: "", description: "" }],
+  },
+  {
+    id: "4",
+    name: "Laíne",
+    cpf: generateId(),
+    dateOfBirth: "19/07/1989",
+    balance: 0,
+    bankStatement: [{ value: 0, date: "", description: "" }],
+  },
+  {
+    id: "5",
+    name: "Tiago",
+    cpf: generateId(),
+    dateOfBirth: "19/07/1989",
+    balance: 0,
+    bankStatement: [{ value: 0, date: "", description: "" }],
+  },
+  {
+    id: "6",
+    name: "Lucas",
+    cpf: generateId(),
     dateOfBirth: "19/07/1989",
     balance: 0,
     bankStatement: [{ value: 0, date: "", description: "" }],
--- modulo5/projeto-sistema-bancario/sistema-bancario/src/index.ts
+++ modulo5/projeto-sistema-bancario/sistema-bancario/src/index.ts
@@ -1,13 +1,14 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import { AddressInfo } from "net";
-import { bankUsers } from "./data";
+
+import { bankUsers, Account } from "./data";
 //Middlewares
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/accounts", (req: Request, res: Response) => {
+app.get("/users/accounts", (req: Request, res: Response) => {
   try {
     const users = bankUsers;
     /* const getByCPF = users.map((u) => {
@@ -35,7 +36,7 @@ app.get("/balance/:name", (req: Request, res: Response) => {
   }
 });
 
-app.get("/balance/:cpf", (req: Request, res: Response) => {
+/* app.get("/balance/:cpf", (req: Request, res: Response) => {
   try {
     const users = bankUsers;
     // const name = req.params.name;
@@ -49,7 +50,7 @@ app.get("/balance/:cpf", (req: Request, res: Response) => {
   } catch (error) {
     res.status(400).end("Cliente não encontrado");
   }
-});
+}); */
 
 app.listen(3003, () => {
   console.log("Alo Adriano, ta me ouvindo");
