202830525ed8e370b9c54f230da7eabf31793cb6
Caio Porto <caioportoramos@gmail.com>
2022-06-23 14:49:32 -0300

    Até o EX 3 OK. Estou com dificuldades no post

--- modulo5/knex-js/atividade-knex-js/src/index.ts
+++ modulo5/knex-js/atividade-knex-js/src/index.ts
@@ -7,109 +7,141 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-//Ex 1-A:
-//Imprime um objeto com os dados do ator de id 001
+const getAllActors = async (): Promise<any> => {
+  const result = await connection.raw(`
+  SELECT * FROM actor 
+  `);
+  return result[0];
+};
+
+app.get("/actor", async (req: Request, res: Response) => {
+  try {
+    const actor = await getAllActors();
+    console.log(actor);
+    res.status(200).send(actor);
+  } catch (error: any) {
+    console.log(error.message);
+    res.status(500).send(`Erro inesperado.`);
+  }
+});
+
+//EX1-A:
 const getActorById = async (id: string): Promise<any> => {
   const result = await connection.raw(`
-  SELECT * FROM actor WHERE id = '${id}'
+  SELECT * FROM actor WHERE id = '${id}'  
   `);
   return result[0][0];
 };
 
-app.get("/actors/:id", async (req: Request, res: Response) => {
-  try {
-    const id = req.params.id;
-    console.log(await getActorById(id));
-    res.end();
-  } catch (error: any) {
-    console.log(error.message);
-    res.status(500).send("Unexpected error");
-  }
-});
-
-//Ex 1-B:
+//EX 1-B:
 
 const getActorByName = async (name: string): Promise<any> => {
   const result = await connection.raw(`
-  SELECT * FROM actor WHERE name = "${name}"`);
-  return result;
+  SELECT * FROM actor WHERE name = '${name}'
+  `);
+  return result[0][0];
 };
 
-app.get("/actors/name/:name", async (req: Request, res: Response) => {
-  try {
-    const name = req.params.name;
-    console.log(await getActorByName(name));
-    res.end();
-  } catch (error: any) {
-    console.log(error.message);
-    res.status(500).send("Unexpected error");
-  }
-});
-
-//Ex 1-C:
+//EX 1-C:
 
 const getActorByGender = async (gender: string): Promise<any> => {
-  const result = connection.raw(`
-  
-  SELECT * FROM actor WHERE gender = "${gender}"`);
+  const result = await connection.raw(`
+  SELECT COUNT(*) as count FROM actor WHERE gender = '${gender}'
+    
+  `);
+  const count = result[0][0].count;
+  return count;
+};
+//EX 2-A:
+
+const updateSalary = async (salary: number, id: string): Promise<any> => {
+  await connection("actor").update({ salary: salary }).where("id", id);
+};
+
+//EX2-B:
+const deleteActor = async (id: string): Promise<any> => {
+  await connection("actor").delete().where("id", id);
+};
+
+//EX2-C:
+
+const averageSalary = async (gender: string, salary: number): Promise<any> => {
+  const result = await connection("actor")
+    .avg("average as salary")
+    .where({ gender });
+  return result[0].average;
+};
+
+//EX3-A:
+
+app.get("/actor/:id", async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
+    const actor = await getActorById(id);
+    console.log(actor);
+    res.status(200).send(actor);
+  } catch (error: any) {
+    console.log(error.message);
+    res.status(500).send(`Erro inesperado.`);
+  }
+});
+
+//EX3-B:
+app.get("/actor", async (req: Request, res: Response) => {
+  try {
+    const gender = req.query.gender;
+    const actorByGender = await getActorByGender(`${gender}`);
+    console.log(actorByGender);
+    res.status(200).send(actorByGender);
+  } catch (error: any) {
+    console.log(error.message);
+    res.status(500).send(`Erro inesperado.`);
+  }
+});
+
+const createActor = async (
+  id: string,
+  name: string,
+  salary: number,
+  birth_date: Date,
+  gender: string
+): Promise<any> => {
+  const result = await connection.raw(`
+  INSERT INTO actor ('${id}', '${name}', '${salary}', '${birth_date}', '${gender}' )
+    
+  `);
+
   return result;
 };
 
-//EX 2-A:
-app.put("/actors/:id", async (req: Request, res: Response) => {
+app.post("/actor", async (req: Request, res: Response) => {
+  try {
+    await createActor(
+      req.body.id,
+      req.body.name,
+      Number(req.body.salary),
+      new Date(req.body.birth_date),
+      req.body.gender
+    );
+
+    res.status(200).send();
+  } catch (error: any) {
+    console.log(error.message);
+    res.status(500).send(`Erro inesperado.`);
+  }
+});
+
+/* app.patch("/actor/:id", async (req: Request, res: Response) => {
   try {
     await connection("actor")
-      .update({
-        salary: req.body.salary,
-      })
+      .update({ salary: req.body.salary })
       .where({ id: req.params.id });
-    res.send("Success!");
+    res.status(200).send("Success!");
   } catch (error: any) {
     console.log(error.message);
-    res.status(500).send(`An unexpected error occurred`);
+    res.status(500).send("Algo deu errado.");
   }
-});
-//EX 2-B:
-app.delete("/actors/delete/:id", async (req: Request, res: Response) => {
-  try {
-    await connection("actor").delete().where({ id: req.params.id });
-    res.send("Ator/Atriz deletado(a) com sucesso!");
-  } catch (error: any) {
-    console.log(error.message);
-    res.status(500).send(`An unexpected error occurred`);
-  }
-});
-
-//Ex 2-C:
-
-app.get("/actors/gender", async (req: Request, res: Response) => {
-  try {
-    const gender = req.query.gender;
-    const result = await connection("actor")
-      .avg(`salary as average`)
-      .where({ gender });
-    return result[0].average;
-  } catch (error: any) {
-    console.log(error.message);
-    res.status(500).send(`An unexpected error has occurred`);
-  }
-});
-
-//EX 3-A:
-app.get("/actors/:id", async (req: Request, res: Response) => {
-  try {
-    const id = req.params.id;
-    const actor = await getActorById(id)
-    
-    res.status(200).send(actor)
-  } catch (error: any) {
-    console.log(error.message);
-    res.status(400).send(error.message);
-  }
-});
-
-//Ex 3-B:
-
+}); */
 
 const server = app.listen(process.env.PORT || 3003, () => {
   if (server) {
