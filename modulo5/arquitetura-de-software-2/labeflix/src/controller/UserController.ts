8948f579253b998113485850cb9ca2790f75ed8d
Caio Porto <caioportoramos@gmail.com>
2022-07-28 22:17:00 -0300

    Ex1 OK

---
+++ modulo5/arquitetura-de-software-2/labeflix/src/controller/UserController.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from "express";
+import { UserBusiness } from "../business/UserBusiness";
+import { User } from "../types/User";
+
+export class UserController {
+  async create(req: Request, res: Response): Promise<void> {
+    try {
+      const { email, name, password } = req.body;
+
+      const userBusiness = new UserBusiness();
+      await userBusiness.create({ email, name, password });
+
+      res.status(201).send({ message: "Usuário cadastrado com sucesso" });
+    } catch (error: any) {
+      res.status(400).send(error.message);
+    }
+  }
+
+  getAll = async (req: Request, res: Response): Promise<void> => {
+    try {
+      const users = await new UserBusiness().getAll();
+
+      res.status(200).send(users);
+    } catch (error: any) {
+      res.status(400).send(error.message);
+    }
+  };
+}
